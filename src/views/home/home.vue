f564409200bfe8362d244d162b7e7c3d2b78c691
jinghaihan <jinghaihan@ebupt.com>
2022-11-07 15:50:39 +0800

    feat(views): 初始化

---
+++ src/views/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- 首页 -->
+    首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  
+</style>
